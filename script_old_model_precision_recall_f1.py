90091f3780fd942c3f2521e8c51433fd35a2238b
Alexandra Spruit <alexandra.spruit@hotmail.com>
2020-01-18 18:50:20 +0100

    Calculating the precision, recall and f1 for old model and bug fix for prediction performance of old model

---
+++ script_old_model_precision_recall_f1.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from data import Preprocessing, Statistics
+
+
+""" This is a script that stores the prediction performance accuracy scores per dialogue act given different input 
+    settings and sequence lengths. The data is read in from .csv files containing the predictions of different models 
+    and input settings.
+
+    The variables that need to be specified:
+        weighted           = chosen from {"weighted", "unweighted"} to specify which model's predictions are to be used.
+        sequence_lengths   = the sequence length with which the model made the predictions.
+        input_settings     = the input settings used: a subset from 
+                             {'dialogue act', 'speaker', 'level', 'utterance length'}. The list must consist of 
+                             abbreviations in the format '_<first letter>', for example ['_d', '_d_u'], which uses first 
+                             only dialogue acts and then dialogue acts and utterance lengths.
+
+    The script outputs csv files containing the accuracy scores of the dialogue acts (rows) per level and accuracy 
+    metric (columns).       
+"""
+
+models = ['unweighted', 'weighted']
+sequence_length = 3
+
+# Reads in the data containing the predictions of a model under the given settings.
+filename = 'analyses/old_model_sequence_length_3_test_set_predictions.csv'
+data = Preprocessing(filename)
+statistics = Statistics(data)
+
+# Gets the precision, recall and f1-score for every dialogue act for different model input settings.
+for weighted in models:
+    accuracy_dict = dict()
+    for dialogue_act in data.DAs:
+        columns = ['labels_' + weighted, 'predictions_' + weighted]
+        precision, recall, f1 = statistics.precision_recall_f1(data.data, columns, dialogue_act)
+
+        if 'all_levels' not in accuracy_dict.keys():
+            accuracy_dict['all_levels'] = dict()
+            accuracy_dict['all_levels']['p'] = dict()
+            accuracy_dict['all_levels']['r'] = dict()
+            accuracy_dict['all_levels']['f1'] = dict()
+        accuracy_dict['all_levels']['p'][dialogue_act] = precision
+        accuracy_dict['all_levels']['r'][dialogue_act] = recall
+        accuracy_dict['all_levels']['f1'][dialogue_act] = f1
+
+        for level in data.levels:
+            level_data = data.data[data.data['level'] == level]
+            precision, recall, f1 = statistics.precision_recall_f1(level_data, columns, dialogue_act)
+
+            if 'level_' + str(level) not in accuracy_dict.keys():
+                accuracy_dict['level_' + str(level)] = dict()
+                accuracy_dict['level_' + str(level)]['p'] = dict()
+                accuracy_dict['level_' + str(level)]['r'] = dict()
+                accuracy_dict['level_' + str(level)]['f1'] = dict()
+            accuracy_dict['level_' + str(level)]['p'][dialogue_act] = precision
+            accuracy_dict['level_' + str(level)]['r'][dialogue_act] = recall
+            accuracy_dict['level_' + str(level)]['f1'][dialogue_act] = f1
+
+    # Code below adapted from https://stackoverflow.com/questions/13575090/construct-pandas-dataframe-from-items-in-nested-dictionary
+    # Prepares the separate parts of the dictionary to be stored in one DataFrame.
+    level_accuracies = []
+    frames = []
+    for level_accuracy, accuracy_scores in accuracy_dict.items():
+        level_accuracies.append(level_accuracy)
+        frames.append(pd.DataFrame.from_dict(accuracy_scores, orient='index'))
+
+    # Stores all the accuracy scores for all the levels over all the dialogue acts into a DataFrame.
+    accuracies = pd.concat(frames, keys=level_accuracies).T.round(4)
+
+    # Saves the accuracy DataFrame to a .csv file.
+    accuracy_file = 'analyses/old_' + weighted + '_model_sequence_length_3_accuracy.csv'
+    accuracies.to_csv(accuracy_file)
--- script_old_model_prediction_performance.py
+++ script_old_model_prediction_performance.py
@@ -20,19 +20,20 @@ learning_rate = 0.001
 batch_size = 16
 epochs = 20
 
-# Preprocesses the training and test data for the sequence length.
-preprocessed = Preprocessing('data/DA_labeled_belc_2019.csv')
-preprocessed.save_dialogues_as_matrices(sequence_length=sequence_length, store_index=True)
-preprocessed.save_dialogue_ids()
-preprocessed.save_class_representation()
-data_frame = preprocessed.data
-data = DataSet()
+# Preprocesses the training data for the sequence length.
+preprocessed_train = Preprocessing('data/DA_labeled_belc_2019.csv')
+preprocessed_train.save_dialogues_as_matrices(sequence_length=sequence_length, store_index=True)
+preprocessed_train.save_dialogue_ids()
+preprocessed_train.save_class_representation()
+train_data = DataSet()
 
-# Makes a train and test split.
-train_data = pd.read_csv('data/train_belc_das_2020.csv')
-train_ids = sorted(list(set(train_data['dialogue_id'])))
-test_data = pd.read_csv('data/test_belc_das_2020.csv')
-test_ids = sorted(list(set(test_data['dialogue_id'])))
+# Preprocesses the test data for the sequence length.
+preprocessed_test = Preprocessing('data/DA_labeled_belc_2019.csv')
+preprocessed_test.save_dialogues_as_matrices(sequence_length=sequence_length, store_index=True)
+preprocessed_test.save_dialogue_ids()
+preprocessed_test.save_class_representation()
+data_frame = preprocessed_test.data
+test_data = DataSet()
 
 # Makes predictions for the weighted and unweighted model and stores them.
 for weighted in models:
@@ -49,12 +50,10 @@ for weighted in models:
                  hidden_nodes=hidden_nodes, n_layers=1, n_classes=13, input_classes=input_classes)
 
     # Trains the model on the training set.
-    data.set_dialogue_ids(train_ids)
-    train(model, data, learning_rate, batch_size, epochs, weighted)
+    train(model, train_data, learning_rate, batch_size, epochs, weighted)
 
     # Tests the model on the test set and stores the labels and predictions.
-    data.set_dialogue_ids(test_ids)
-    labels_predictions, accuracy = evaluate(model, data, save_labels_predictions=True)
+    labels_predictions, accuracy = evaluate(model, test_data, save_labels_predictions=True)
     labels_predictions = pd.DataFrame(labels_predictions.reshape(-1, 3))
 
     # Stores the labels and predictions in a DataFrame.
@@ -65,12 +64,12 @@ for weighted in models:
     input_frame = input_frame.astype(str)
 
     # Replaces all the numerical values of the labels and predictions with their name.
-    DAs = preprocessed.DAs
+    DAs = preprocessed_train.DAs
     for i in range(len(DAs)):
         input_frame = input_frame.replace({str(i) + '.0': DAs[i]})
 
     # Adds the labels and predictions for test set as columns to the original test data in one DataFrame.
-    test_data = test_data.merge(input_frame, how='left', left_index=True, right_index=True)
+    data_frame = data_frame.merge(input_frame, how='left', left_index=True, right_index=True)
 
 # Saves the DataFrame containing all the labels and predictions for the different input settings.
-test_data.to_csv('analyses/old_model_sequence_length_3_test_set_predictions.csv')
+data_frame.to_csv('analyses/old_model_sequence_length_3_test_set_predictions.csv')
